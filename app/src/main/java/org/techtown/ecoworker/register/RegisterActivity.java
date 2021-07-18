be7ff8364d7f8675bdf34f6627b4a38818675243
MinYeongPark <yingo24655@naver.com>
2021-07-18 17:18:58 +0900

    Delete log test

--- app/src/main/java/org/techtown/ecoworker/register/RegisterActivity.java
+++ app/src/main/java/org/techtown/ecoworker/register/RegisterActivity.java
@@ -89,7 +89,6 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String userID = idname.getText().toString();
                 String userPass = passwd2.getText().toString();
-                Log.d("asdf", "잘나옴2-1");
 
                 //콜백 처리부분(volley 사용을 위한 ResponseListener 구현 부분)
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
@@ -97,7 +96,6 @@ public class RegisterActivity extends AppCompatActivity {
                     //서버로부터 여기서 데이터를 받음
                     public void onResponse(String response) {
                         try {
-                            Log.d("asdf", "잘나옴2-2");
                             //서버로부터 받는 데이터는 JSON타입의 객체이다
                             JSONObject jsonObject = new JSONObject(response);
                             //그중 Key값이 "success"인 것을 가져온다.
@@ -113,13 +111,11 @@ public class RegisterActivity extends AppCompatActivity {
                                 return;
                             }
                         } catch (JSONException e) {
-                            Log.d("asdf", "잘나옴-catch");
                             e.printStackTrace();
                         }
 
                     }
                 }; //responseListener 끝
-                Log.d("asdf", "잘나옴-끝남");
                 //서버로 Volley를 이용해서 요청을 함.
                 //1. RequestObject를 생성한다. 이때 서버로부터 데이터를 받을 responseListener를 반드시 넘겨준다.
                 RegisterRequest registerRequest = new RegisterRequest(userID, userPass, responseListener);
@@ -127,7 +123,6 @@ public class RegisterActivity extends AppCompatActivity {
                 RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
                 //3. RequestQueue에 RequestObject를 넘겨준다.
                 queue.add(registerRequest);
-                Log.d("asdf", "잘나옴-완전끝");
             }
         });
     }
